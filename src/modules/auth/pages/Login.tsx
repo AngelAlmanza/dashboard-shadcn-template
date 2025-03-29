55fb1863ec0aa262bb90b62a9782085f5618659f
Angel Almanza <almanza.angel245@gmail.com>
2025-03-29 12:42:49 -0700

    Minor fix for protected routes

--- src/modules/auth/pages/Login.tsx
+++ src/modules/auth/pages/Login.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@/core/components/ui/button"
+import { useAppDispatch } from "@/core/store/hooks"
+import { setIsAuthenticated } from "@/core/store/slices"
+import { useNavigate } from "react-router-dom"
 
 function Login() {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    localStorage.setItem("isAuthenticated", "true")
+    dispatch(setIsAuthenticated(true))
+    navigate("/")
+  }
+
   return (
     <div>
-      <Button>Login</Button>
+      <Button onClick={handleLogin}>Login</Button>
     </div>
   )
 }
